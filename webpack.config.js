518255fb981a38a3a98d156df096efba756a59ff
meerajahmed <meerajahmed@gmail.com>
2017-12-28 22:43:24 +0530

    fixed typo and code indentation

--- webpack.config.js
+++ webpack.config.js
@@ -4,29 +4,29 @@ var ExamplePlugin = require("./ExamplePlugin");
 var webpack = require("webpack");
 module.exports = {
   // entry: first file webpack starts at when it creates dependency graph
-	entry: "./src/index.js",
+  entry: "./src/index.js",
   // output: how and where to put the bundles and how to format them
-	output: {
-		// bundle.js has webpack run time code which enables browsers to run module in browser
-		filename: "bundle.js",
-		// __dirname is absolute path of current dir
-		path: path.join(__dirname, "dist")
-	},
+  output: {
+    // bundle.js has webpack run time code which enables browsers to run module in browser
+    filename: "bundle.js",
+    // __dirname is absolute path of current dir
+    path: path.join(__dirname, "dist")
+  },
   // loaders: performs some type of transformation on single file (input source) just before it is added to dependency graph
-  // They help us treat any type of asset to as module in dependency graph
-	module: {
-	  // rules: how to treat file before they are added to the dependency graph
-	  rules: [
+  // They help us treat any type of asset as module in dependency graph
+  module: {
+    // rules: how to treat file before they are added to the dependency graph
+    rules: [
       {
-        test: /\.png/,
+        test: /\.png$/,
         use: ["file-loader"]
       }
     ]
   },
   // plugins: can perform any of the functionality that loaders could't
-  // if we want perform any of the operation of multiple files
+  // if we want perform any of the operation on multiple files
   plugins: [
     new ExamplePlugin(),
     new webpack.optimize.UglifyJsPlugin()
   ]
-}
+};
