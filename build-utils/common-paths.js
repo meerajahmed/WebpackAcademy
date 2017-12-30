d9f6dd10c5cf50cf7d5bd7ca1622ac35cb089d1a
meerajahmed <meerajahmed@gmail.com>
2017-12-30 22:40:37 +0530

    common base config

--- build-utils/common-paths.js
+++ build-utils/common-paths.js
@@ -1,6 +1,8 @@
 const commonPaths = require("path");
 
 module.exports = {
+  appEntry: "./src/",
   // __dirname is absolute path of current dir
   outputPath: commonPaths.resolve(__dirname, "../", "dist")
+
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // webpack manages every assets in out application by building dependency graph
 
 import createCard from "./card";
+import "./webpack-logo-on-white-bg.png";
 
 createCard(
   "Web Fundamentals: Webpack config for dev and prod env",
